9073beae02ca578020805c6d19444bac653a9c47
file not found <voodoologic@users.noreply.github.com>
2025-03-22 15:30:33 -0700

    button js

---
+++ app/components/log-in.js
@@ -0,0 +1,11 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class LogIn extends Component {
+  @action
+  handleClick(event){
+    event.preventDefault();
+    this.args.submitAction()
+  }
+  
+}
