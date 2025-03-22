357cc18be0c2bc94d499881eb409107d0ca59a1e
file not found <voodoologic@users.noreply.github.com>
2025-03-22 14:01:47 -0700

    netflify plugin config

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    'ember-cli-netflify': {
+      redirects: [
+        '/* /index.html '
+      ]
+    }
   });
 
   app.import('public/video.css')
